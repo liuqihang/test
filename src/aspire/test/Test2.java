07cf401c122b34acbfe44fe14579a96676c881cf
liuqihang <302728910@qq.com>
2015-12-16 16:59:51 +0800

    update hub code

--- src/aspire/test/Test2.java
+++ src/aspire/test/Test2.java
@@ -2,14 +2,6 @@ package aspire.test;
 
 public class Test2 {
 
-	private static final String USER_NAME="liuqihang";
-
-	public static void main(String[] args) {
-		for (int num = 0; num < 5; num++) {
-			System.out.println(num);
-		}
-	}
-
 	public void println(){
 		String sql = "select * from lxc_user";
 		System.out.println(sql);
@@ -23,6 +15,12 @@ public class Test2 {
 			break;
 		}
 		return false;
+	}
+	
+	public static void main(String[] args) {
+		for (int num = 0; num < 5; num++) {
+			System.out.println(num);
+		}
 	}
 
 
